67e0510eaa4f6c2cf737dbf85b9a66be1dd63b2f
Chafael <rafael_ruiz11@icloud.com>
2025-11-03 01:10:32 -0600

    Corregi para que tenga iconos globales

--- src/app/features/home/components/general-info.component/general-info.component.ts
+++ src/app/features/home/components/general-info.component/general-info.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GeneralStats } from '../../../../core/models/stats.model';
+import { StatsService } from '../../../../core/services/stats.service';
 
 @Component({
-  selector: 'app-general-info.component',
-  imports: [],
+  selector: 'app-general-info', 
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './general-info.component.html',
   styleUrl: './general-info.component.css',
 })
-export class GeneralInfoComponent {
+export class GeneralInfoComponent implements OnInit {
+  stats: GeneralStats = {
+    totalProjects: 0,
+    projectsThisMonth: 0,
+    totalAnalysis: 0,
+    analysisThisMonth: 0
+  };
 
+  loading = true;
+
+  constructor(private statsService: StatsService) {}
+
+  ngOnInit(): void {
+    this.loadStats();
+  }
+
+  loadStats(): void {
+    this.statsService.getGeneralStats().subscribe({
+      next: (data) => {
+        this.stats = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Error cargando estadísticas', err);
+        this.loading = false;
+      }
+    });
+  }
 }
